fe2e071852eef382cccecca849b3288997d1d02d
Paul <paul.hallam@digital.cabinet-office.gov.uk>
2024-07-22 16:24:28 +0100

    still adding statef

---
+++ setup-statef/variables.tf
@@ -0,0 +1,15 @@
+variable "default_tags" {
+    type = map
+    description = "Set of default tags"
+}
+
+variable "tfstate_bucket" {
+    type    = string
+    default = "gccc-misp-tfstate"
+}
+
+variable "dynamodb_table_name" {
+  description = "The name of the DynamoDB table to use for state locking."
+  type        = string
+  default     = "gccc-misp-tfstate-table"
+}
