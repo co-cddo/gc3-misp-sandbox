e9def8722ba27d1947bac88a7b2b6a519782ffab
Paul <paul.hallam@digital.cabinet-office.gov.uk>
2024-07-22 16:00:26 +0100

    Create github-aws-oidc.tf

---
+++ github-aws-oidc.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_openid_connect_provider" "githubOidc" {
+ url = "https://token.actions.githubusercontent.com"
+ client_id_list = [
+   "sts.amazonaws.com"
+ ]
+ thumbprint_list = ["1c58a3a8518e8759bf075b76b750d4f2df264fcd","6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+#
+# Add the role that will allow the githubv user to connect and run the actions.
+#
+data "aws_iam_policy_document" "github_allow" {
+ statement {
+   effect  = "Allow"
+   actions = ["sts:AssumeRoleWithWebIdentity"]
+   principals {
+     type        = "Federated"
+     identifiers = [aws_iam_openid_connect_provider.githubOidc.arn]
+   }
+   condition {
+     test     = "StringLike"
+     variable = "token.actions.githubusercontent.com:sub"
+#    values   = ["repo:${GitHubOrg}/${GitHubRepo}:*"]
+     values   = ["repo:co-cddo/gc3-misp-sandbox:*"]
+   }
+   condition {
+     test     = "StringEquals"
+     variable = "token.actions.githubusercontent.com:aud"
+     values   = ["sts.amazonaws.com"]
+   }
+ }
+}
+resource "aws_iam_role" "github_role" {
+  name               = "GithubActionsRole"
+  assume_role_policy = data.aws_iam_policy_document.github_allow.json
+}
