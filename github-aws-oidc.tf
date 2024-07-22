7b14d2d3ffdb0a0887520ccfaa7a9ccdc564b014
Paul <paul.hallam@digital.cabinet-office.gov.uk>
2024-07-22 08:23:30 +0100

    Update github-aws-oidc.tf

--- github-aws-oidc.tf
+++ github-aws-oidc.tf
@@ -7,3 +7,27 @@ resource "aws_iam_openid_connect_provider" "githubOidc" {
 
  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
 }
+
+#
+# Add the role that will allow the githubv user to connect and run the actions.
+#
+data "aws_iam_policy_document" "github_allow" {
+ statement {
+   effect  = "Allow"
+   actions = ["sts:AssumeRoleWithWebIdentity"]
+   principals {
+     type        = "Federated"
+     identifiers = [aws_iam_openid_connect_provider.githubOidc.arn]
+   }
+   condition {
+     test     = "StringLike"
+ variable = "token.actions.githubusercontent.com:sub"
+     values   = ["repo:${GitHubOrg}/${GitHubRepo}:*"]
+
+   }
+ }
+}
+ resource "aws_iam_role" "github_role" {
+ name               = "GithubActionsRole"
+ assume_role_policy = data.aws_iam_policy_document.github_allow.json
+}
