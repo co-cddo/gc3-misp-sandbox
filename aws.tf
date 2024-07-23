201b79af9cc47bb1bc4fae9f6ceea4257b4c422e
Paul <paul.hallam@digital.cabinet-office.gov.uk>
2024-07-23 08:46:27 +0100

    cleanup of files after reorgsanisation

--- aws.tf
+++ aws.tf
@@ -5,6 +5,12 @@ provider "aws" {
   }
 }
 
+#
+# This module was going to be called to create the AWS IAM Identity Provider to be used by githiub for builds 
+# but as I do not have the priv to create theidentity provider or the required roles I can't use it and will have 
+# to have it by someone else.check 
+# I am keeping it as it may be useful.
+#
 #module identity_provider {
 #  source = "./identity_provider"
 #}
